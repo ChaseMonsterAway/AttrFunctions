c92b83d140345cb1ff3cf59bdd242a3488500851
sunjun <sunjun@leinao.ai>
2022-01-06 09:34:21 +0800

    new:: analyzer: label_distribution

---
+++ attr/analyze.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+
+
+def label_distribution(txt, plot=False):
+    label_dict = defaultdict(int)
+    with open(txt, 'r') as f:
+        for line in f.readlines():
+            labels = line.split(' ')[1:]
+            for label in labels:
+                label_dict[label] += 1
+    if plot:
+        plt.bar(
+            range(len(label_dict)), label_dict.values(), tick_label=list(label_dict.keys())
+        )
+        plt.show()
+
+    return label_dict
